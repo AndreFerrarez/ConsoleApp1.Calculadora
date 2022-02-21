50dd4a3c2896eeac21a22617b15e211d037ae6d9
AndreFerrarez <andre.augusto.17@gmail.com>
2022-02-21 10:42:19 -0300

    Add project files.

---
+++ ConsoleApp4.subtração/Subtração.cs
@@ -0,0 +1,13 @@
+﻿
+int n1, n2, resultado;
+
+Console.WriteLine("Digite um número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite outro número: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+resultado = n1 - n2;
+
+Console.WriteLine(("O resultado é de: ") + (resultado));
+Console.ReadKey();
